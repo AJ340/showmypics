93ae75ce24441457cd63569bd6d677c348d346b3
Andres Quinones <AJ340@users.noreply.github.com>
2021-09-13 21:24:49 -0400

    Added routes for core pages
    After researching express-generator and pug it was decided to switch to basic express for future handlebars template implementation and passport js implementation.

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require('express');
-const app = express();
-const port = 3000
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/', function (req, res) {
+  console.log("Got a POST request for the homepage");
+  res.send('Hello POST');
 })
 
-app.listen(port, ()=> {
-	console.log('Example app listening at http://localhost${port}')
+app.get('/addImages', function(req, res) {
+  // Use Handlebars to generate images page
+  res.sendFile(path.join(__dirname, 'addImages.html'))
 })
+
+app.get('/imgs', function(req, res) {
+  // Use Handlebars to generate images page
+  res.send("Load User images here.")
+})
+
+
+var server = app.listen(3000, function() {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+});
