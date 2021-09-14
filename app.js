9a49aaee19fb24840df78df73652706b97e7a824
Andres Quinones <AJ340@users.noreply.github.com>
2021-09-13 22:37:31 -0400

    Implemented login GET and POST
    Moved html files to static folder
    Implemented dummy login post

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
+
+// GET requests
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
-app.post('/', function (req, res) {
-  console.log("Got a POST request for the homepage");
-  res.send('Hello POST');
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '/static/login.html'));
 })
 
 app.get('/addImages', function(req, res) {
@@ -24,9 +27,25 @@ app.get('/imgs', function(req, res) {
 })
 
 
+// POST requests
+app.post('/login', function (req, res) {
+  //Insert login code here
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
+  //res.redirect('/users/' + req.user.username);
+})
+
+app.post('/', function (req, res) {
+  console.log("Got a POST request for the homepage");
+  res.send('Hello POST');
+})
+
+
+
 var server = app.listen(3000, function() {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log(`Example app listening at http://${host}:${port}`);
 });
